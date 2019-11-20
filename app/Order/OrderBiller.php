3356183cf5a44b95158f2262ec8ee9bf7d5941d2
Mohamad Fazel <mdhesari99@gmail.com>
2019-11-20 13:43:31 +0330

    document orderBiller

--- app/Order/OrderBiller.php
+++ app/Order/OrderBiller.php
@@ -7,6 +7,14 @@ use app\Illuminate\BillerInterface;
 class OrderBiller implements BillerInterface
 {
 
+    /**
+     * generate a new bill
+     *
+     * @param  int $id
+     * @param  int $amount
+     *
+     * @return void
+     */
     public function bill(int $id, int $amount)
     {
 
